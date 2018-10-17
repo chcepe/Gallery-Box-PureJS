a9bd41e31598d14af010456ce09a23390d1ea18b
chcepe <christian@techoneglobal.com>
2018-10-17 12:25:48 +0800

    Added files

---
+++ custom.js
@@ -0,0 +1,19 @@
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function displayPhotos(data) {
+    let photoHTML = "";
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        photoHTML += '<ul>';
+        for (var i = 0; i < data.length; i++) {
+            photoHTML += '<li>';
+            photoHTML += '<img id="thumb" src="' + data[i].urls.regular + '">';
+            photoHTML += '</li>';
+        }
+        photoHTML += '</ul>';
+    }
+    var photoUl = document.getElementById("photos");
+    if (photoUl != null) photoUl.innerHTML = photoHTML;
+};
+xhr.open("GET", "https://api.unsplash.com/photos/search?per_page=10&client_id=cf4bdc51d49dcb014d8277b937132495c150a251d117ba0f8283eb5bcaf65951&query=philippines&orientation=landscape");
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.send();
---
+++ gbox.js
@@ -0,0 +1,57 @@
+// EVENTS ON CLICK
+var current_photo = 1;
+var images = null;
+document.addEventListener("click", function (e) {
+    if (e.srcElement.id == "thumb") {  // ON THUMB CLICK
+        images = (images == null ? document.getElementById("photos").getElementsByTagName("img").length : images);
+
+
+        //ORDERING PHOTOS INDEX
+        var photos = document.getElementById("photos").getElementsByTagName("li");
+        for (var i = 0, len = photos.length; i < len; i++) {
+            (photos[i].firstChild.setAttribute("n", (i + 1)));
+        }
+
+        current_photo = (parseInt(e.target.getAttribute("n")));
+        document.body.innerHTML += '<div id="gallery-overlay"><div class="photo"><span id="info">Hello</span><span id="close" title="Close">x</span><img src="https://i.imgur.com/mNWmIxZ.png" title="Previous" id="prev"><img src="https://i.imgur.com/pY5gK7d.png" title="Next" id="next"><img id="photo" src="' + e.srcElement.src + '"></div></div>';
+        npButtons();
+    } else if (e.srcElement.id == "close") { // ON CLOSE BUTTON CLICK
+        document.getElementById("gallery-overlay").remove();
+    } else if (e.srcElement.id == "next" || e.srcElement.id == "prev") { // ON PREV OR NEXT BUTTONS CLICK
+        if (e.srcElement.id == "next") {
+            current_photo++;
+        } else {
+            current_photo--;
+        }
+        var elements = document.getElementById("photos").getElementsByTagName("li");
+        var src = (elements.item(current_photo - 1).getElementsByTagName("img")[0].getAttribute("src"));
+        document.getElementById("photo").src = src;
+        npButtons();
+    }
+});
+
+
+// UPDATE PREV AND NEXT BUTTONS
+function npButtons() {
+    document.getElementById("info").innerHTML = "<b>" + current_photo + "</b> / " + images;
+    document.getElementById("prev").style.display = (current_photo == 1 || images == 0) ? "none" : "block";
+    document.getElementById("next").style.display = (current_photo == images || images == 0) ? "none" : "block";
+}
+
+
+// ALLOW PREV OR NEXT NAVIGATION FROM KEYBOARD
+document.onkeydown = checkKey;
+function checkKey(e) {
+    e = e || window.event;
+    if (document.getElementById("gallery-overlay")) {
+        if (e.keyCode == "37") { // ON KEYDOWN LEFT ARROW
+            if (document.getElementById("prev").style.display != "none") document.getElementById("prev").click();
+        }
+        else if (e.keyCode == "39") { // ON KEYDOWN RIGHT ARROW
+            if (document.getElementById("next").style.display != "none") document.getElementById("next").click();
+        }
+        else if (e.keyCode == "27") { // ON KEYDOWN ESCAPE
+            document.getElementById("gallery-overlay").remove();
+        }
+    }
+}
